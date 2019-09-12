051cb2d2eead877d1638f1de0a45c26e1a4c7ab3
Joshua Colicchio <joshuacolicchio@gmail.com>
2019-09-12 19:45:31 -0400

    Updated Database System
    Added a method for executing raw queries
    Reformatted existing methods to use the raw query method
    Altered Shelter.db animals table to store ArrivalDate
    Added a debug method for getting all animals of a given species from the database

--- src/Database/Database.java
+++ src/Database/Database.java
@@ -6,6 +6,13 @@ import java.sql.*;
 
 public class Database {
 
+    public static ResultSet getAllOfSpecies(String species) {
+        ResultSet rs = null;
+
+        rs = executeQuery("SELECT * FROM animals WHERE MainSpecies = '" + species + "'", null, false);
+        return rs;
+    }
+
     /**
      * Loads all animals from the database.
      */
@@ -102,5 +109,96 @@ public class Database {
         catch (Exception ex) {
             System.out.println("Database Exception: Failed to save animal.\nReason: " + ex.toString());
         }
+    }
+
+    // this method is kinda wild right now, I will simplify it more as we develop the program more, but for now
+    // it's a general work horse for querying the database
+    /**
+     * Used to execute custom queries to the database.
+     * @param query The query code to execute. (eg. INSERT INTO animals (Name, MainSpecies) VALUES (?,?)
+     * @param queryParams Array of objects that match the query code. (eg. new Object[] {"John", "Canine"}
+     * @return Returns the generated key fetched from the query, if applicable. Returns -1 if no generated keys are
+     * retrieved.
+     */
+    public static ResultSet executeQuery(String query, Object[] queryParams, boolean fetchKey) {
+        try{
+            Connection connection = DriverManager.getConnection("jdbc:sqlite:src/database/shelter.db");
+
+            if (connection != null) {
+
+                // build query using method
+                PreparedStatement pstmt = buildQuery(connection, query, queryParams);
+
+                // send query & return results
+                if (fetchKey) {
+                    // if fetchKey is true, we are most likely using an INSERT statement, so we're not looking for
+                    // a row, but rather just the generated key from the new row, so we only need to return that.
+                    return pstmt.getGeneratedKeys();
+                }
+                else {
+                    // but if fetchKey is false, we're most likely searching the database for something, so we want to
+                    // return the entire table of results, so that the user can search through the results themselves.
+
+                    // Depending on whether or not the query will return a result, or simply succeed/fail, you have to
+                    // call executeQuery, or executeUpdate. For our uses, I think we'll only need to care about SELECT.
+                    if (query.contains("SELECT")) {
+                        return pstmt.executeQuery();
+                    }
+                    else {
+                        // Since other commands like INSERT and UPDATE don't return anything when executed, return null.
+                        pstmt.executeUpdate();
+                        return null;
+                    }
+                }
+            }
+            else {
+                System.out.println("Database Exception: Failed to execute query.\nReason: Connection invalid.");
+            }
+        }
+        catch (ArrayIndexOutOfBoundsException ex) {
+            System.out.println("Database Exception: Failed to execute query \"" + query + "\"\n" +
+                    "Reason: Array Index Out Of Bounds. Ensure the number of ?'s match the number of params.");
+        }
+        catch (Exception ex) {
+            System.out.println("Database Exception: Failed to execute query \"" + query + "\"\n" +
+                    "Reason: " + ex.toString());
+        }
+
+        // If we reach this return, the query failed and the console should have output an error for us.
+        return null;
+    }
+
+    private static PreparedStatement buildQuery(Connection connection, String query, Object[] queryParams) {
+        try {
+            // We use a PreparedStatement rather than a regular statement so that we can add our variables below
+            PreparedStatement pstmt = connection.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS);
+
+            if (queryParams != null && queryParams.length > 0) {
+
+                // build the query by iterating through all queryParams passed to this method.
+                // for each parameter passed to the query, replace a '?' with the queryParam object at that index
+                for (int i = 0; i < queryParams.length; i++) {
+                    if (queryParams[i] instanceof String) {
+                        pstmt.setString(i + 1, (String) queryParams[i]);
+                    } else if (queryParams[i] instanceof Integer) {
+                        pstmt.setInt(i + 1, (int) queryParams[i]);
+                    } else if (queryParams[i] instanceof Double) {
+                        pstmt.setDouble(i + 1, (double) queryParams[i]);
+                    } else if (queryParams[i] instanceof Float) {
+                        pstmt.setFloat(i + 1, (float) queryParams[i]);
+                    } else {
+                        System.out.println("UNKNOWN TYPE: " + queryParams[i].getClass().getTypeName());
+                    }
+                }
+            }
+
+            return pstmt;
+        }
+        catch (Exception ex) {
+            System.out.println("Database Exception: Error building query \"" + query + "\"\nReason: " + ex.toString());
+        }
+
+        // if we reach this return, an error occurred, and the console should output an exception.
+        return null;
     }
 }
