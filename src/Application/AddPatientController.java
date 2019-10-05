061b07e9608fabfbd46f0ee62cd3d7cb2014dcae
rc9207 <rc9207@gmail.com>
2019-10-04 21:19:59 -0400

    10/14/2019
    - Added choices to sexChoiceBox.
    - Removed Style options from FXML and replaced with common CSS sheet called Style.css.

--- src/Application/AddPatientController.java
+++ src/Application/AddPatientController.java
@@ -2,6 +2,8 @@ package Application;
 
 import java.io.IOException;
 import java.util.Calendar;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -17,74 +19,85 @@ import Database.Database;
 
 public class AddPatientController {
 
-    @FXML
-    private Button dashboardButton;
-    @FXML
-    private Button patientsButton;
-    @FXML
-    private TextField newPatientName;
-    @FXML
-    private TextField newPatientSpecies;
-    @FXML
-    private ChoiceBox<?> newPatientSex;
-    @FXML
-    private TextField newPatientColor;
-    @FXML
-    private TextField newPatientBreed;
-    @FXML
-    private TextField newPatientAge;
-    @FXML
-    private TextField newPatientMicrochip;
-    @FXML
-    private Button savePatientButton;
+  private ObservableList sexChoiceBox = FXCollections.observableArrayList("Male",
+      "Female");
 
-    // Return to Dashboard
-    public void dashboardButtonPushed(ActionEvent event) throws IOException {
-        Parent dashboardParent = FXMLLoader.load(getClass().getResource("/Dashboard.fxml"));
-        Scene dashboardScene = new Scene(dashboardParent);
+  @FXML
+  private Button dashboardButton;
+  @FXML
+  private Button patientsButton;
+  @FXML
+  private TextField newPatientName;
+  @FXML
+  private TextField newPatientSpecies;
+  @FXML
+  private ChoiceBox<String> newPatientSex;
+  @FXML
+  private TextField newPatientColor;
+  @FXML
+  private TextField newPatientBreed;
+  @FXML
+  private TextField newPatientAge;
+  @FXML
+  private TextField newPatientMicrochip;
+  @FXML
+  private Button savePatientButton;
 
-        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        window.setScene(dashboardScene);
-        window.show();
+  // Load String values into Sex ChoiceBox.
+  @FXML
+  private void initialize() throws Exception {
+    newPatientSex.setItems(sexChoiceBox);
+}
+
+  // Return to Dashboard
+  public void dashboardButtonPushed(ActionEvent event) throws IOException {
+    Parent dashboardParent = FXMLLoader.load(getClass().getResource("/Dashboard.fxml"));
+    Scene dashboardScene = new Scene(dashboardParent);
+
+    Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    window.setScene(dashboardScene);
+    window.show();
+  }
+
+  // Return to Patients Scene
+  public void patientsButtonPushed(ActionEvent event) throws IOException {
+    Parent patientsParent = FXMLLoader.load(getClass().getResource("/Patients.fxml"));
+    Scene patientsScene = new Scene(patientsParent);
+
+    Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    window.setScene(patientsScene);
+    window.show();
+  }
+
+  public void savePatientButtonPushed(ActionEvent event) throws IOException {
+
+    // @TODO Have the appropriate text field change to red when the form is submitted but the field is empty.
+    // @TODO Same thing as any other form does when you don't fill in required information
+    if (newPatientName.getText().isEmpty() || newPatientName.getText().compareTo(" ") == 0) {
+      System.out.println("Name field required");
+    }
+    if (newPatientSpecies.getText().isEmpty() || newPatientSpecies.getText().compareTo(" ") == 0) {
+      System.out.println("Species field required");
+    }
+    if (newPatientColor.getText().isEmpty() || newPatientColor.getText().compareTo(" ") == 0) {
+      System.out.println("Color field required");
+    }
+    if (newPatientBreed.getText().isEmpty() || newPatientBreed.getText().compareTo(" ") == 0) {
+      System.out.println("Breed field required");
+    }
+    if (newPatientAge.getText().isEmpty() || newPatientAge.getText().compareTo(" ") == 0) {
+      System.out.println("Age field required");
+    }
+    if (newPatientMicrochip.getText().isEmpty()
+        || newPatientMicrochip.getText().compareTo(" ") == 0) {
+      System.out.println("Microchip field required");
     }
 
-    // Return to Patients Scene
-    public void patientsButtonPushed(ActionEvent event) throws IOException {
-        Parent patientsParent = FXMLLoader.load(getClass().getResource("/Patients.fxml"));
-        Scene patientsScene = new Scene(patientsParent);
-
-        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        window.setScene(patientsScene);
-        window.show();
-    }
-
-    public void savePatientButtonPushed(ActionEvent event) throws IOException {
-
-        // @TODO Have the appropriate text field change to red when the form is submitted but the field is empty.
-        // @TODO Same thing as any other form does when you don't fill in required information
-        if (newPatientName.getText().isEmpty() || newPatientName.getText().compareTo(" ") == 0) {
-            System.out.println("Name field required");
-        }
-        if (newPatientSpecies.getText().isEmpty() || newPatientSpecies.getText().compareTo(" ") == 0) {
-            System.out.println("Species field required");
-        }
-        if (newPatientColor.getText().isEmpty() || newPatientColor.getText().compareTo(" ") == 0) {
-            System.out.println("Color field required");
-        }
-        if (newPatientBreed.getText().isEmpty() || newPatientBreed.getText().compareTo(" ") == 0) {
-            System.out.println("Breed field required");
-        }
-        if (newPatientAge.getText().isEmpty() || newPatientAge.getText().compareTo(" ") == 0) {
-            System.out.println("Age field required");
-        }
-        if (newPatientMicrochip.getText().isEmpty() || newPatientMicrochip.getText().compareTo(" ") == 0) {
-            System.out.println("Microchip field required");
-        }
-
-        // Passed the other checks, so create new animal in database
-        Database.saveNewPatient(newPatientName.getText(), newPatientSpecies.getText(), "TEMPERAMENT HERE",
-                newPatientSex.toString(), newPatientColor.getText(), newPatientBreed.getText(),
-                newPatientMicrochip.getText(), newPatientAge.getText(), "WEIGHT HERE",
-                java.util.Calendar.getInstance().getTime().toString(), "ADOPTABLE HERE");
-    }
+    // Passed the other checks, so create new animal in database
+    Database
+        .saveNewPatient(newPatientName.getText(), newPatientSpecies.getText(), "TEMPERAMENT HERE",
+            newPatientSex.toString(), newPatientColor.getText(), newPatientBreed.getText(),
+            newPatientMicrochip.getText(), newPatientAge.getText(), "WEIGHT HERE",
+            java.util.Calendar.getInstance().getTime().toString(), "ADOPTABLE HERE");
+  }
 }
